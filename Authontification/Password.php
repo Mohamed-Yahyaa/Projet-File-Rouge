b5c096ca2e422aac7ed658414caa1d174a13ac18
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-06-23 13:10:19 +0100

    Prototype Authentification update

---
+++ Authontification/Password.php
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+<?php
+// include 'AuthenticationManager.php';
+// if (!empty($_POST)){
+// $firstName = $_POST['firstName'];
+// $email = $_POST['email'];
+
+//     $gestion = new AuthenticationManager(); 
+
+//   if( $data= $gestion->password( $email,$firstName)){
+
+    
+
+
+
+// foreach($data as $value){
+
+   
+//     $message=  "Hello ".$value->getFirstname().' '. $value->getLastname()." Your password is: ".$value->getPassword();
+  
+
+// }
+
+
+// if(
+// mail($email, "Sujet",$message)
+// ){
+//   echo "email envoyer";
+// }
+// }
+// else{
+//   echo "non";
+// }
+// }
+
+?>
+
+<a href="login.php">back</a>
+<form action=""method='POST'>
+
+firstName    <input type="text" name="firstName">
+  Email <input type="email" name="email">
+    <button type="submit"> afficher</button>
+ 
+
+    
+  
+</form>
